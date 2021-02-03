e05e8361dca7ecca5a6539127762abea10f5f114
Hanwe Lee <mobiledev@kakao.com>
2021-02-03 10:25:15 +0900

    1. Root 생성

---
+++ RIBsSample/RIBsSample/Root/RootViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RootViewController.swift
+//  RIBsSample
+//
+//  Created by hanwe on 2021/02/03.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol RootPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class RootViewController: UIViewController, RootPresentable, RootViewControllable {
+
+    weak var listener: RootPresentableListener?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
